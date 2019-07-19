a215f6b6f856cc488b884259816845ff95d684de
Jason Loomis <jason@jrloomis.com>
2019-07-18 20:53:03 -0400

    github api working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,16 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios
+  .get("https://api.github.com/users/jrloom")
+  .then(resolve => console.log(resolve))
+  .catch(error => console.log(error));
 
+// axios
+// .get("https://dog.ceo/api/breed/bulldog/images/random/3")
+// .then(axiosData => console.log(axiosData))
+// .catch(err => console.log(err));
+//
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
